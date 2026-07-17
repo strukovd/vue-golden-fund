12c5fb98ac72d24de399b12e0bdee156a0f89922
Dmitriy <68727449+strukovd@users.noreply.github.com>
2026-07-17 17:17:15 +0600

    Refactor BaseIsland component to use setup syntax

--- vue/components/BaseIsland.vue
+++ vue/components/BaseIsland.vue
@@ -1,60 +1,43 @@
 <template>
 	<div class="base-island">
-		<h2 v-if="title" class="bi-title">
-			<v-icon v-if="prependIcon" :icon="prependIcon" size="1.2em" style="margin-right:.3em; opacity:.7;"></v-icon>
+		<h4 v-if="title" class="bi-title">
+			<BaseIcon v-if="prependIcon" :name="prependIcon" size="1.2em" style="margin-right:.4em; color:#2563ea;"></BaseIcon>
 			<span>{{ title }}</span>
-		</h2>
-		<div class="content">
-			<slot name="default"></slot>
-		</div>
+		</h4>
+		<slot name="default"></slot>
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import BaseIcon from './BaseIcon.vue';
 
-export default defineComponent({
-	name: 'BaseIsland',
-	props: {
-		title: String,
-		prependIcon: String
-	},
-	data() {
-		return {};
-	},
-	methods: {},
+const props = defineProps({
+	title: String,
+	prependIcon: String
 });
 </script>
 
 <style lang="scss">
 .base-island {
 	background-color:#ffffff;
-	padding:1.6em;
-	border-radius:6px;
-	margin:.6em;
-	// border:1px solid #f3f3f3; // #e0e2e791
-	// box-shadow: 0 0 3px 0 rgba(0,0,0,.05);
-	box-shadow: 0px 0px 30px rgba(3, 100, 165, 0.1);
+	border:1px solid #e5e5e5;
+	padding:1.2em;
+	border-radius:12px;
+	// margin-top:1em;
 
-	&:not(:last-child) {
-		margin-bottom: 1.6em;
-	}
+	// border: 1px solid rgba(37, 99, 234, 0.12);
+	// box-shadow: 0 18px 42px rgba(37, 99, 234, 0.08);
+	// border-radius: 24px;
 
 	.bi-title {
+		display: flex;
+    	align-items: center;
 		color: #172b4d;
-		font-weight: 400;
-		margin-bottom: 1em;
-		background: #fdfdfd;
-		border-radius: 5px;
-		// border: 1px solid #f0f0f0;
-		padding:.2em 0;
-	}
-
-	>.content {
-		// background-color: #fdfdfd;
-		// padding:1.6em;
-		// border-radius:6px;
-		// border:1px solid #f3f3f3;
+		font-weight: 700;
+		font-size:1em;
+		border-bottom: 1px solid #eff6ff;
+		padding-bottom: 1rem;
+		margin-bottom: 1rem;
 	}
 }
 </style>
